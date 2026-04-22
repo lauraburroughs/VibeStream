d4190efa93151bbfbfc3fa8b50ae7b9066e6a923
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-04-22 04:59:18 -0500

    Initial Commit

---
+++ VibeStream/VibeStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeStreamApp.swift
+//  VibeStream
+//
+//  Created by Laura Burroughs on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct VibeStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
